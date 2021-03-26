ba8567be97a0babc92734fd11cf924a70a357c3f
Peter Szatmary <peter.szatmary@gmail.com>
2021-03-27 00:49:13 +0100

    first commit.

---
+++ src/main/java/com/kryptotipy/cryptoiscrypto/CryptoiscryptoApplication.java
@@ -0,0 +1,13 @@
+package com.kryptotipy.cryptoiscrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoiscryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoiscryptoApplication.class, args);
+	}
+
+}
